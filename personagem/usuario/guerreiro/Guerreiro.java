6852de87160468e84fbe6f5239dae930e035e1b6
cauaandradevieira <cauaandradevieiraa@gmail.com>
2025-01-29 15:52:37 -0300

    fiz e testei o metodo defender

--- personagem/usuario/guerreiro/Guerreiro.java
+++ personagem/usuario/guerreiro/Guerreiro.java
@@ -1,5 +1,6 @@
 package projeto_rpg.personagem.usuario.guerreiro;
 
+import projeto_rpg.personagem.Personagem;
 import projeto_rpg.personagem.usuario.Usuario;
 import projeto_rpg.personagem.usuario.guerreiro.dados_guerreiros.DadosGuerreiros;
 
@@ -20,12 +21,38 @@ public class Guerreiro extends Usuario
         this.critico = dadosGuerreiros.getCritico();
         this.escudo = dadosGuerreiros.getEscudo();
         this.dadosGuerreiros = dadosGuerreiros;
-        this.chanceDefender = dadosGuerreiros.getChanceEscudo();
+        this.chanceDefender = 4;
         this.escudo = dadosGuerreiros.getEscudo();
     }
 
-    public void defender()
+    public void defender(Personagem inimigo)
     {
+        inimigo.setPermissaoAtacar(true);
+        int numeroAleatorio = random.nextInt(1,6);
 
+        if(numeroAleatorio <= chanceDefender)
+        {
+            System.out.println("DEFENDEU");
+
+            System.out.println("VIDA DO PERSONAGEM: " + this.vida);
+            int danoinimigoAntesAlterar = inimigo.getDano();
+            System.out.println("DANO INIMIGO SEM ALTERAR: " + danoinimigoAntesAlterar);
+            inimigo.setDano( danoinimigoAntesAlterar / 2);
+            System.out.println("DANO INIMIGO ALTERANDO O VALOR: " + inimigo.getDano());
+            inimigo.atacar(this);
+            System.out.println("VIDA DO PERSONAGEM: " + this.vida);
+            inimigo.setDano(danoinimigoAntesAlterar);
+            System.out.println("DANO DO INIMIGO ALTERADO COMO ANTES: " + inimigo.getDano());
+
+            System.out.println(inimigo.getPermissaoAtacar());
+            inimigo.setPermissaoAtacar(false);
+            System.out.println(inimigo.getPermissaoAtacar());
+        }
+    }
+
+    @Override
+    public void atacar(Personagem monstro) {
+        super.atacar(monstro);
+        defender(monstro);
     }
 }
