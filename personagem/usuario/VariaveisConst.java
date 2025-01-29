880a4fab9547d5bc80098cd2933827873d5df449
cauaandradevieira <cauaandradevieiraa@gmail.com>
2025-01-29 15:47:33 -0300

    removi uma const que nao precisa estar nessa interface

--- personagem/usuario/VariaveisConst.java
+++ personagem/usuario/VariaveisConst.java
@@ -2,8 +2,6 @@ package projeto_rpg.personagem.usuario;
 
 public interface VariaveisConst
 {
-    boolean PERMISSAO_ATACAR_INICIAL = true;
-
     float BARRA_LEVEL_INICIAL = 0f;
     float BARRA_LEVEL_MAXIMO = 100f;
 
